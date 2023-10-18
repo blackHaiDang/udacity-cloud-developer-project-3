e9de642a7a26e907384213a11b3b0c1d415b5f35
blackHaiDang <nguyenhaidang.simple@gmail.com>
2023-10-18 22:11:10 +0700

    everything is okay except hpa

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aeef67050bef642b8b3826ecb92aa3c3-1386367370.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
