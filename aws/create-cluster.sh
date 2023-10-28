474f2fac09dd99217dc512475117f42a85854144
blackHaiDang <nguyenhaidang.simple@gmail.com>
2023-10-28 08:37:27 +0700

    set env before build

--- aws/create-cluster.sh
+++
@@ -1,9 +0,0 @@
-﻿eksctl create nodegroup \
-  --cluster project3-cluster \
-  --region us-east-1 \
-  --name project3-cluster\
-  --node-ami-family ami-family \
-  --node-type m5.large \
-  --nodes 2 \
-  --nodes-min 2 \
-  --nodes-max 3 \
