7b65dcd125aba4f151775fc19084b15c44e23f3c
blackHaiDang <nguyenhaidang.simple@gmail.com>
2023-10-28 08:53:57 +0700

    change aws profile and check docker compose config

--- set_env.sh
+++ set_env.sh
@@ -6,6 +6,6 @@ export POSTGRES_HOST=postgres.cw9a3mp3kerk.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=udacity-project3-cloud-developer
 export AWS_REGION=us-east-1
-export AWS_PROFILE=admin
+export AWS_PROFILE=role-user
 export JWT_SECRET=testing
 export URL=http://localhost:8100
